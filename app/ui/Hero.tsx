78ca428bfc715ac94cc1810305ecd38f6d65bc8a
Tony <federicoaguirre134@gmail.com>
2025-01-25 13:57:13 -0600

    added some height to text on hero

--- app/ui/Hero.tsx
+++ app/ui/Hero.tsx
@@ -4,10 +4,10 @@ import WomanStance from '@/public/images/WomanStance.png'
 
 function Hero() {
   return (
-    <div className="bg-lime h-full w-full grid grid-cols-1 md:grid-cols-2 place-items-center px-20">
+    <div className="bg-lime min-h-[775] w-full grid grid-cols-1 lg:grid-cols-2 place-items-center px-20">
       <div className="flex flex-col justify-center items-start p-8 h-full gap-10 px-8">
-        <h1 className={`${rufina.className} text-greenDark text-7xl`}>Discover and Find Your Own Fashion!</h1>
-        <p className='text-green prose font-semibold'>
+        <h1 className={`${rufina.className} text-greenDark text-7xl/normal`}>Discover and Find Your Own Fashion!</h1>
+        <p className='text-green prose text-base/loose font-semibold'>
           Explore our curated collection of stylish clothing and accessories
           tailored to your unique taste.
         </p>
