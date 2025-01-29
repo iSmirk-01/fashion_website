77f90496ad2e69469c8a458d743223ce26746908
Tony <federicoaguirre134@gmail.com>
2025-01-28 18:48:08 -0600

    image bug fix 2.0

--- app/components/CardShowCase.tsx
+++ app/components/CardShowCase.tsx
@@ -12,7 +12,7 @@ function CardShowCase({ src, title, text, imgCss, cardCss}: Props) {
 
   return (
     <div className={`${cardCss} center-flex gap-4`}>
-      <Image src={src} alt={title} className={`${imgCss} w-[430] h-[566]`} />
+      <Image src={src} alt={title} className={`${imgCss} w-[430px] h-[566px]`} />
       <div className='center-flex text-gray-600 gap-2'>
         <h4 className='font-bold text-2xl'>{title}</h4>
         <p className='text-center max-w-md font-semibold'>{text}</p>
